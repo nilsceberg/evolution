d88e41d91fb587f967865f20e38282725c72c841
Nils Ceberg <nils.ceberg@gmail.com>
2022-04-11 02:10:03 +0200

    simulation: randomly walking agents

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::ops::Range;
 use num_derive::{FromPrimitive, ToPrimitive};
 use rand::Rng;
+use uuid::Uuid;
 
 mod dot;
 mod viewer;
@@ -62,6 +63,32 @@ impl Brain {
     }
 }
 
+pub struct Agent {
+    uuid: Uuid,
+    position: (f32, f32),
+}
+
+impl Agent {
+    fn new() -> Agent {
+        let mut rng = rand::thread_rng();
+        Agent {
+            uuid: Uuid::new_v4(),
+            position: (
+                (rng.gen::<f32>() * 2.0 - 1.0) * 300.0,
+                (rng.gen::<f32>() * 2.0 - 1.0) * 300.0,
+            ),
+        }
+    }
+
+    fn simulate(&mut self) {
+        let mut rng = rand::thread_rng();
+        let dx = (rng.gen::<f32>() * 2.0 - 1.0) * 10.0;
+        let dy = (rng.gen::<f32>() * 2.0 - 1.0) * 10.0;
+        self.position.0 += dx;
+        self.position.1 += dy;
+    }
+}
+
 fn main() {
     use simplelog::{CombinedLogger, TermLogger, LevelFilter, Config, TerminalMode, ColorChoice};
 
@@ -79,8 +106,18 @@ fn main() {
 
     //brain.draw_graph(&mut std::io::stdout());
 
+    let mut agents : Vec<Agent> = vec![];
+    for _ in 0..100 {
+        agents.push(Agent::new());
+    }
+
+    publisher.send(viewer::spawn(&agents)).unwrap();
     loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        publisher.send(viewer::Event::Clear).unwrap(); 
+        std::thread::sleep(std::time::Duration::from_millis(50));
+        publisher.send(viewer::frame(&agents)).unwrap(); 
+
+        for agent in &mut agents {
+            agent.simulate();
+        }
     }
 }
