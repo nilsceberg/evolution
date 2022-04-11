74a79923a092e46e7f8f20297df19a7bed98ea1c
Nils Ceberg <nils.ceberg@gmail.com>
2022-04-11 15:55:23 +0200

    viewer: implement some hacky 'genome' colorization

--- viewer/src/World.tsx
+++ viewer/src/World.tsx
@@ -7,13 +7,26 @@ interface WorldProps {
 }
 
 interface AgentProps {
+    info: AgentInfo;
     position: [number, number];
 }
 
+function agentColor(genome: string): string {
+    let hue = genome
+        .split("")
+        .map(x => (x.charCodeAt(0) - 97) / 25)
+        .map((x, i) => Math.sin(i / 10) * x)
+        .reduce((a,b) => a+b, 0);
+    return `hsl(${hue * 120 % 360}deg 100% 50%)`;
+}
+
 const Agent = (props: AgentProps) => {
+    const color = agentColor(props.info[0]);
     const style = {
         marginLeft: props.position[0] - 4,
         marginTop: props.position[1] - 4,
+        backgroundColor: color,
+        boxShadow: `0px 0px 20px 5px ${color}`,
     };
 
     return (
@@ -24,8 +37,10 @@ const Agent = (props: AgentProps) => {
 export const World = (props: WorldProps) => {
     return (
         <div className="World">
+            <div className="XAxis"/>
+            <div className="YAxis"/>
             {props.agents.map((agent, index) => 
-                <Agent key={agent[0]} position={props.frame[index]}/>
+                <Agent key={agent[0]} info={agent} position={props.frame[index]}/>
             )}
         </div>
     );
